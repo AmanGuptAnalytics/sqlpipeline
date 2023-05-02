a353dd6d7ca040d4aaef28224a2f8132ac741128
AmanGuptAnalytics <98139823+AmanGuptAnalytics@users.noreply.github.com>
2023-05-02 03:12:41 +0000

    SQL_Pipeline

---
+++ sql_database_pipeline.py
@@ -0,0 +1,18 @@
+from typing import List
+
+import dlt
+
+from sql_database import sql_database
+
+if __name__ == '__main__':
+    pipeline = dlt.pipeline(
+        pipeline_name="sql_tables",
+        destination='bigquery',
+        dataset_name='sql_database_data',
+        full_refresh=False,
+    )
+    data = sql_database()
+    info = pipeline.run(data)
+    print(info)
+
+
