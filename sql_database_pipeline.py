c19beb916ee426e19bc5cb48d49ddf2a725bd12e
AmanGuptAnalytics <98139823+AmanGuptAnalytics@users.noreply.github.com>
2023-05-08 11:59:51 +0530

    Update sql_database_pipeline.py

--- sql_database_pipeline.py
+++ sql_database_pipeline.py
@@ -1,18 +1,21 @@
+from sql_database import sql_database,sql_table
 from typing import List
-
+from dlt.common.configuration.specs import ConnectionStringCredentials
 import dlt
 
-from sql_database import sql_database
 
-if __name__ == '__main__':
-    pipeline = dlt.pipeline(
-        pipeline_name="sql_tables",
-        destination='bigquery',
-        dataset_name='sql_database_data',
-        full_refresh=False,
-    )
-    data = sql_database()
-    info = pipeline.run(data)
-    print(info)
+def sql_to_destination() -> None:
+    database = sql_database()
+   
+    # load a single table
+    table_name = sql_table(table='table_name')
+    
+    pipeline = dlt.pipeline(pipeline_name="sql_tables", destination='bigquery', dataset_name='sql_database_inc_load1', full_refresh=False )
+   
+    data = table_name
+    print(pipeline.run(data))
+    
 
 
+if __name__ == '__main__':   
+    sql_to_destination()
