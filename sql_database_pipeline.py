c7fedb658ba17c25d0f3fccd1aab6765b600a54a
AmanGuptAnalytics <98139823+AmanGuptAnalytics@users.noreply.github.com>
2023-05-08 12:03:30 +0530

    Update sql_database_pipeline.py

--- sql_database_pipeline.py
+++ sql_database_pipeline.py
@@ -7,7 +7,7 @@ import dlt
 def sql_to_destination() -> None:
     database = sql_database()
    
-    # load a single table
+    # load a single table, replace the table_name with the name of table you want to load
     table_name = sql_table(table='table_name')
     
     pipeline = dlt.pipeline(pipeline_name="sql_tables", destination='bigquery', dataset_name='sql_database_inc_load1', full_refresh=False )
